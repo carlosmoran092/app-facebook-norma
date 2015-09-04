4c161e873471811fe2131de5f621e951e005aeab
Carlos Moran <camoranns@gmail.com>
2015-09-04 08:33:18 -0500

    Cambios app.js
    edición archivo app

--- app/js/app.js
+++ app/js/app.js
@@ -3,9 +3,12 @@ $(".portada" ).click(function() {
   alert( oID ); 
   var idPadre = $(this).parents("span:first").attr("id");
   alert(idPadre);
-  $(this).parents("span:first").css('background', '#444').append("<span class='float:right'>Cerrar</span>");
+  $(this).parents("span:first").css('background', '#444')/*aparecer label para cancelar selección*/;
 
   // agregar a arreglo de respuestas positivas si atributo x es positivo si es negativo, ira en negativo.
+// Si click en label cancelar
+ // Quitar selección (quitar color de fondo)
+ //eliminar del arreglo si respuesta es negativa
 });
 
 
