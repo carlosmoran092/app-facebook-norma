9dae7b8a249231db8f1f1401d8a9f318cb0438af
Carlos Moran <camoranns@gmail.com>
2015-09-07 10:59:59 -0500

    Cambios 7-915

--- app/js/app.js
+++ app/js/app.js
@@ -1,31 +1,40 @@
+var tipo_pregunta = true;
 var positivo = new Object();
 var negativo = new Object();
-
 positivo.seleccionadas = [];
+
 $(".portada" ).click(function() {
   var id = $(this).attr("id");
-  
-  if (positivo.id!=id) {
-	  alert( id ); 
+    var activo = $(this).attr('data-active');
+  alert(activo);
+
+
+
+//$(this).attr('data-active', 'true');
+
+  if (activo == undefined) {
+  	activo = $(this).attr('data-active', 'true');
+	  alert(id); 
+	  
 	  var idPadre = $(this).parents("span:first").attr("data-name");
 	  alert(idPadre);
-	  $(this).parents("span:first").css('background', '#C6E7F6') /*aparecer label para cancelar selección*/;
-	   var Arrseleccion = positivo.seleccionadas; conReg = positivo.seleccionadas[idPadre]
-	   if(conReg != idPadre){
-	   	Arrseleccion.push(idPadre);
-	   }else{
-	   	alert("Ya esta creado");
-	   }
+	  $(this).parents("span:first").css('background', '#C6E7F6');
+	   var Arrseleccion = positivo.seleccionadas; conReg = positivo.seleccionadas[idPadre];
+	   
+	   if(conReg != idPadre)
+	   {Arrseleccion.push(idPadre);}
+
 	   
 	  alert(positivo.seleccionadas);
-	  // agregar a arreglo de respuestas positivas si atributo x es positivo si es negativo, ira en negativo.
-	  // Si click en label cancelar
-	  // Quitar selección (quitar color de fondo)
-	  //eliminar del arreglo si respuesta es negativa
-	  
-	 /*----------  Código para boton de cancelar selección  ----------*/
+	}else if(activo == "true"){
+		alert("deseleccionar")
+		activo = $(this).removeAttr('data-active');
+		$(this).parents("span:first").css('background', '#fff');
+		eliminarItem=positivo.seleccionadas.indexOf(idPadre);
+		alert(eliminarItem);
+		delete positivo.seleccionadas[eliminarItem];
 
-  } else{alert("nada pasa")};
+	} else{alert("nada pasa")};
 
  
 });
